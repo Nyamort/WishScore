c23948ebefc395c7b334fb59e850bedc32288369
Mgt03 <max.g03@outlook.fr>
2023-05-31 08:14:08 +0200

    Login op et fixture insérées

--- screens/CompetitionScreen.tsx
+++ screens/CompetitionScreen.tsx
@@ -1,18 +1,29 @@
-import { FlatList } from "react-native";
+import { FlatList,Text } from "react-native";
 import CompetitionListItem from "../components/CompetitionListItem";
+import React from "react";
+import {CommonActions} from "@react-navigation/native";
 export const CompetitionList : string[] = [
     "Ligue1",
     "Ligue2",
     "LigueEuropa",
     "ChampionsLeague"
 ]
-export default function CompetitionScreen(){
-   
+export default function CompetitionScreen({navigation,dispatch}){
+    var sport;
+    var nav = navigation.dispatch(navigation.getParent().insertBeforeLast('Home', {item: sport}));
 
-    return (
-        <>
-            <FlatList data={CompetitionList} renderItem={elt => <CompetitionListItem name={elt.item}/>}
-                keyExtractor={(item) => item}/>
-        </>
-    );
-}
+    var onChangeValue = ({viewableItems}) => {
+        if (viewableItems.length > 0) {
+            sport = viewableItems[0].item;
+        }
+    }
+
+        return (
+            <>
+                <Text>{sport}</Text>
+                <FlatList data={CompetitionList} renderItem={elt => <CompetitionListItem name={elt.item}/>}
+                          keyExtractor={(item) => item} onViewableItemsChanged={dispatch()}/>
+
+            </>
+        );
+    }
