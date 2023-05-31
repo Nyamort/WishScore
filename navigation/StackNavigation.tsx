6d58cf9f2c10ca61cdbd18e7418bbba7d3c5c723
Adrien Coudour <adrien.coudour@etu.uca.fr>
2023-05-31 15:51:42 +0200

    Ajout d'une navigation pour les match et ajout de package

---
+++ navigation/StackNavigation.tsx
@@ -0,0 +1,31 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet} from "react-native";
+import HomeScreen from "../screens/HomeScreen";
+import MatchScreen from "../screens/MatchScreen";
+
+export default function StackNavigation(){
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Match" component={MatchScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection:"row",
+        width:"100%",
+        alignItems: 'flex-end',
+        justifyContent: 'space-between'
+    },
+
+    button:{
+
+    }
+});
